853d7545cbcadd68d26734f75410a27958105873
cesartalves <cesartalvez@gmail.com>
2025-12-12 10:30:41 -0300

    Start Ecommerce from Scratch:
    - Create Login
    - Generate Admin::Login and Admin::Dashboard

---
+++ app/controllers/admin/login_controller.rb
@@ -0,0 +1,25 @@
+class Admin::LoginController < ApplicationController
+
+  def login
+    @user = User.new
+  end
+
+  def create
+    # Find user by email
+    @user = User.find_by(email: params[:user][:email])
+
+    # Validate credentials using Devise method + admin check
+    if @user && @user.valid_password?(params[:user][:password]) && @user.admin?
+      sign_in(@user)
+      redirect_to admin_dashboard_path, notice: "Welcome, admin!"
+    else
+      flash[:alert] = "Invalid credentials or not an admin."
+      render :login, status: :unauthorized
+    end
+  end
+
+  def destroy
+    sign_out(current_user)
+    redirect_to admin_login_path, notice: "Logged out."
+  end
+end
